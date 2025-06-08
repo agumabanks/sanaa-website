b760f9029c28a3dcc3dab44382cafb3bb724ea07
Aguma Banks <agumabanksibrahim@gmail.com>
2025-06-08 16:28:23 +0300

    Add placeholder page templates

---
+++ resources/views/pages/prices.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.landing')
+
+@section('content')
+    <section class="py-12 bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold">Pricing</h1>
+            <p class="mt-4 text-gray-600">Pricing details will be published soon.</p>
+        </div>
+    </section>
+@endsection

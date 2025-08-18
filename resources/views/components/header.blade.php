93c45cb34d06a52876a833eb1754f26e186cc81d
Aguma Banks <sanaaa.co@gmail.com>
2025-08-18 05:54:14 +0300

    feat: add animated hero and theme toggle

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -277,6 +277,15 @@
           Login
         </a>
         @endauth
+
+        <button id="theme-toggle" class="p-2 rounded text-gray-800 hover:text-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 transition" aria-label="Toggle dark mode">
+          <svg class="h-5 w-5 block dark:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m8.66-9h-1M4.34 12h-1m15.02 5.66l-.7-.7M6.34 6.34l-.7-.7m12.02 0l-.7.7M6.34 17.66l-.7.7M12 5a7 7 0 100 14 7 7 0 000-14z" />
+          </svg>
+          <svg class="h-5 w-5 hidden dark:block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+          </svg>
+        </button>
       </div>
 
       <!-- Mobile Menu Button -->
